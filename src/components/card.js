1009993d8ad6d22d14a17e9811396a9009f0741b
Alexandra Rusu <alexandrarusu1611@gmail.com>
2018-07-24 19:30:06 +0300

    styled the buttons

--- src/components/card.js
+++ src/components/card.js
@@ -82,7 +82,7 @@ class Card extends Component{
                 }
                 </div>
 
-                <button type="submit">{this.state.contentVisible ? 'Clear Madlib' : 'Generate Madlib'}</button>
+                <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{this.state.contentVisible ? 'Clear Madlib' : 'Generate Madlib'}</button>
 
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : ''
